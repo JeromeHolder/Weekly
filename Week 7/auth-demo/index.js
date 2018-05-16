c54c0e1990053370d703cb37c3ebde606efeaf93
Jerome Holder <j.e.holder@outlook.com>
2018-05-15 20:26:31 -0400

    daily update

--- Week 7/auth-demo/index.js
+++ Week 7/auth-demo/index.js
@@ -1,8 +1,12 @@
 const express = require('express'),
       app = express(),
-      bcrypt = require('bcrypt');
+      bcrypt = require('bcrypt'),
+      jwt = require('jsonwebtoken');
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+// Secret key for jwt
+let secret_key = "4e4dad6e88ceeb0e5a927c57ae7d3680af65f5fdecc36d4fd33f05e8bd640c4b";
 
 // Mimic a DB of users
 const usersDB = [];
@@ -46,9 +50,44 @@ app.post('/login', (req, res)=>{
             return res.status(401).send('Incorrect username or password');  //Do not send back too much info on what the error is (providing too much info aids malicious intent)
         }
         else {
-            return res.send('Success!');
+            // Now that we have authenticated the user, let's generate a JWT for them and send it back
+            let payload = {
+                iss: 'fishfindr',
+                exp: (Date.now() + 24*60*60*1000),
+                subject: user.email
+            };
+            jwt.sign(payload, secret_key, (err, token)=>{
+                if(err){
+                    return res.status(500).send('Error making token.');
+                }
+                return res.json({token: token});
+            });
         }
     });
+});
+
+// Middleware
+authorize = (req, res, next) =>{
+    // Do some processing on the request, then either send a response or allow it to continue to its next processing fn
+    // First check if the reqeust has a token, if not, respond with 401 Unauthorized
+    let token = req.headers.authorization;
+    // If there is no token
+    if(!token){
+        return res.status(401).send('Need a token');
+    }
+    // If there is a token we need to check it
+    jwt.verify(token, secret_key, (err, payload)=>{
+        // If there is an error it means the token was bad
+        if(err){
+            return res.status(401).send('Bad token');
+        }
+        // If we get here it means the request has a good token, so we can allow it to proceed to the final handler with next()
+        next();
+    });
+}
+
+app.get('/privatefishes', authorize, (req, res)=>{
+    res.send("Don't eat my fish!");
 });
 
 app.listen(8080, ()=>{console.log('Server running on 8080')});
--- Week 7/jwt-practice/index.js
+++ Week 7/jwt-practice/index.js
@@ -1,2 +1,30 @@
 const jwt = require('jsonwebtoken');
 
+let secret_key = "4e4dad6e88ceeb0e5a927c57ae7d3680af65f5fdecc36d4fd33f05e8bd640c4b";
+
+// Imagine we just authenticated a user, johnny football
+let payload = {
+    iss: 'holder',
+    subject: 'johnnyfootball@cool.go',
+    exp: (Date.now() + 1000*60*60*24)
+};
+
+jwt.sign(payload, secret_key, (err, token)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    // We created our token, this is what we send back as the response to our /login route
+    console.log(token);
+
+    jwt.verify(token, secret_key, (err, decodedPayLoad)=>{
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log(decodedPayLoad);
+    });
+});
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJob2xkZXIiLCJzdWJqZWN0Ijoiam9obm55Zm9vdGJhbGxAY29vbC5nbyIsImV4cCI6MTUyNjQ4MzMxNTE2NywiaWF0IjoxNTI2Mzk2OTE1fQ.OeoS9e1KYc5XOwYUzwzuDzK0PPXxjn6sK4Oa9HrESWA
